664169190063e2f22822c8c59aa434d7659d5e46
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-07-22 18:03:55 +0200

    update header component with user data

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,13 +2,13 @@ import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
-import { useDispacth } from "react-redux";
+import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
 import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
   const auth = getAuth(app);
-  const dispatch = useDispacth();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleGoogleClick = async () => {
     const provider = new GoogleAuthProvider();
